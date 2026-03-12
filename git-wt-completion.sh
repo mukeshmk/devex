8037919b6988aee4bcf894e4a90af609ba5fb3ce
Mukesh Arambakam <amukesh.mk@gmail.com>
2026-03-12 17:46:07 +0000

    WIP: added auto-completion code snippet

---
+++ git-wt-completion.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Auto-completion logic for 'git wt'
+# Git's master completion script automatically looks for functions named _git_<subcommand>
+
+_git_wt() {
+    # The list of all available commands
+    local subcommands="clone add clean list lock move prune repair unlock"
+    
+    # compgen is a built-in bash tool that filters the subcommands based on what the user typed so far
+    COMPREPLY=( $(compgen -W "$subcommands" -- "${COMP_WORDS[COMP_CWORD]}") )
+}
